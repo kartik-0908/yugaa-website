5e08b73eb1396dea83ea74d5e4ebc7efb3b2f663
Kartik <kartik_a@mt.iitr.ac.in>
2024-10-01 16:46:48 +0530

    changes

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useState } from 'react';
-import { Card, CardHeader, CardContent, CardFooter } from '@/components/ui/card';
+import { Card, CardHeader, CardContent } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 
